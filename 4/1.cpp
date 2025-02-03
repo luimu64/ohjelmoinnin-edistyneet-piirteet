487b7f4b3b07d24a19ce2365553303a67c4b8457
luimu64 <luimu@luimu.dev>
2025-02-03 21:44:32 +0200

    4/1

---
+++ 4/1.cpp
@@ -0,0 +1,11 @@
+/*
+Suurin staattinen taulukko, jonka voin luoda on 8.3 megatavua.
+Suurin dynaaaminen taulukko, jonka voin luoda on noin 49.9 gigatavua.
+*/
+
+int main() {
+    char arr[8300000];
+    char* pArr = new char[49989999999];
+
+    return 0;
+}
