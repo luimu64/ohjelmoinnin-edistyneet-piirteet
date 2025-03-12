afdfd3acf671831fee6561e58bc2049798ad5208
luimu64 <luimu@luimu.dev>
2025-03-12 10:11:39 +0200

    removed last excercise main function contents

--- Juha/2/Henkilo/main.cpp
+++ Juha/2/Henkilo/main.cpp
@@ -3,17 +3,6 @@
 using namespace std;
 
 int main() {
-    Henkilo henkilo1;
-    Henkilo henkilo2;
-
-    henkilo1.setNimi("Juha");
-    henkilo1.setIka(25);
-    henkilo2.setNimi("Matti");
-    henkilo2.setIka(30);
-
-    henkilo1.tulostaHenkilonTiedot();
-    henkilo2.tulostaHenkilonTiedot();
-
     Henkilo pekka;
     pekka.tulostaHenkilonTiedot();
 
--- Juha/2/Paivays/main.cpp
+++ Juha/2/Paivays/main.cpp
@@ -1,29 +1,6 @@
 #include "Paivays.h"
 
 int main() {
-    Paivays tanaan, huomenna;
-    tanaan.setPaiva(5);
-    tanaan.setKuukausi(3);
-    tanaan.setVuosi(2024);
-
-    huomenna.setPaiva(6);
-    huomenna.setKuukausi(3);
-    huomenna.setVuosi(2024);
-
-    tanaan.tulostaPaivays();
-    huomenna.tulostaPaivays();
-
-    Paivays uusiPaiva;
-    uusiPaiva.setPaiva(28);
-    uusiPaiva.setKuukausi(2);
-    uusiPaiva.setVuosi(2023);
-
-    uusiPaiva.tulostaPaivays();
-    uusiPaiva.addPaiva();
-    uusiPaiva.tulostaPaivays();
-    uusiPaiva.addPaiva();
-    uusiPaiva.tulostaPaivays();
-
     Paivays constructoriPaiva(28, 2, 2023);
     constructoriPaiva.tulostaPaivays();
 
